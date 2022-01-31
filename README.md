6c7271eb5244d4c8541bfcc62c8f0860c2238e01
Nikegamerjjjj <myftr12394@gmail.com>
2022-01-31 16:11:24 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# testing-terminal
+# Testing Terminal
+
+**This is a special terminal made with Python**
+
+**Write `help` if you cant understand correct commands, or you dont know how to use.**
--- terminal.py
+++ terminal.py
@@ -9,3 +9,16 @@ print(f"\n\n\nTesting Terminal v{version}, Build {build_date}, userId: {getpass.
 
 while running:
     commands = input(f"{getpass.getuser()} [-> ")
+    if "vim" in commands:
+        try:
+            os.system("vim")
+        except SyntaxError:
+            print("Vim isn't installed")
+
+    if "say" in commands:
+        sayword = input("What to say? ")
+        print(f"\n\n\n\{sayword}")
+
+    if "help" in commands:
+        print("You can write 'say': to print what you wrote")
+        print("'vim': to run Vi IMporved")
